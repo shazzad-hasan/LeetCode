fc3f47529de7b493419b7f6158aa8048542930fc
Shazzad Hasan <shazzadraihan@gmail.com>
2022-02-02 22:00:32 +0600

    add 162

---
+++ python/Find_Peak_Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        first = 0
+        last = len(nums)-1
+        while first < last:
+            mid = (first+last) // 2
+            if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]:
+                return mid
+            elif nums[mid] < nums[mid+1]:
+                first = mid + 1
+            else:
+                last = mid - 1
+        return last
